9413f58e1bc5dfbf2008bc4fb3da0a28b809bc85
02022003nha <02022003nha@gmail.com>
2025-04-19 14:13:01 +0700

    update CRUD category management

--- BE/src/controllers/danhMuc.ts
+++ BE/src/controllers/danhMuc.ts
@@ -19,6 +19,70 @@ const storeDanhMuc = async (req: any, res: any) => {
     });
   }
 };
+const updateDanhMuc = async (req: any, res: any) => {
+  try {
+    const _id = req.params;
+    const data = req.body;
 
+    await danhMucService.updateDanhMuc(_id, data);
 
-export { storeDanhMuc };
+    res.status(200).json({
+      message: "Danh mục đã được cập nhật thành công",
+    });
+  } catch (error: any) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+
+const getData = async (req: any, res: any) => {
+  try {
+
+    const data = await danhMucService.getDataDanhMuc();
+
+    res.status(200).json({
+      status: "200",
+      data: data,
+    });
+  } catch (error: any) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+
+const deleteAll = async (req: any, res: any) => {
+  try {
+
+    await danhMucService.deleteAllDanhMuc();
+    res.status(200).json({
+      status: "200",
+      message: "Đã xóa tất cả danh mục thành công!",
+    });
+  } catch (error: any) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+
+const deleteById = async (req: any, res: any) => {
+  const { id } = req.params;
+  try {
+
+    await danhMucService.deleteByIdaDanhMuc({ id });
+
+    res.status(200).json({
+      status: "200",
+      message: "Đã xóa thành công!",
+    });
+  } catch (error: any) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+
+export { storeDanhMuc, updateDanhMuc, getData, deleteAll, deleteById };
+
--- BE/src/routers/danhMuc.ts
+++ BE/src/routers/danhMuc.ts
@@ -1,8 +1,18 @@
 import { Router } from "express";
-import {storeDanhMuc} from "../controllers/danhMuc";
+import {
+    storeDanhMuc,
+    updateDanhMuc,
+    getData,
+    deleteAll,
+    deleteById,
+  } from "../controllers/danhMuc";
+import { accessTokenAdmin } from "../middlewares/admin.middleware";
 const router = Router();
 
 router.post("/create",storeDanhMuc);
-
+router.post("/update/:id", accessTokenAdmin, updateDanhMuc);
+router.get("/getAll", getData);
+router.delete("/delete/all", accessTokenAdmin, deleteAll);
+router.delete("/delete/:id", accessTokenAdmin, deleteById);
 
 export default router;
