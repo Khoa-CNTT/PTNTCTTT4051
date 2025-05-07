de06002f00799335b5f275fcac707216eca1faed
phamvantu090303 <phamtu090303@gmail.com>
2025-05-07 21:04:42 +0700

    fix: index file and contract model

--- BE/src/routers/hopdong.ts
+++ BE/src/routers/hopdong.ts
@@ -6,10 +6,10 @@ import {
   extendContract,
 } from "../controllers/hopDong";
 import { accessTokenValidatetor } from "../middlewares/user.middleware";
-const router = express.Router();
+const routerHopDong = express.Router();
 
-router.get("/customer", accessTokenValidatetor, customer);
-router.post("/create", accessTokenValidatetor, createContract);
-router.get("gia_han_hop_dong/:ma_phong", extendContract);
-router.get("/detail", accessTokenValidatetor, detailContract);
-export default router;
+routerHopDong.get("/customer", accessTokenValidatetor, customer);
+routerHopDong.post("/create", accessTokenValidatetor, createContract);
+routerHopDong.get("gia_han_hop_dong/:ma_phong", extendContract);
+routerHopDong.get("/detail", accessTokenValidatetor, detailContract);
+export default routerHopDong;
